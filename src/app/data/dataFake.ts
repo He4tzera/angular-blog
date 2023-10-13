d555ebe671e31fe848843c19c3a78cb4d473de03
Eduardo Santana <dudu_lima.11@hotmail.com>
2023-10-13 15:32:06 -0300

    Created footer

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake =[
+{
+  "id":"1",
+  "title": "É Anunciada a convocação da Seleção Brasileira",
+  "description":"Fernando Diniz anuncia a convocação da Seleção Brasileira",
+  "photoCover":"https://lncimg.lance.com.br/cdn-cgi/image/width=3840,quality=75,format=webp/uploads/2023/09/53174127059_ad2e89d560_k-aspect-ratio-512-320-1.jpg",
+},
+{
+  "id":"2",
+  "title": "Raphael Veiga é convocado para a Seleção Brasileira",
+  "description":"Ola description 2",
+  "photoCover":"https://a.espncdn.com/photo/2023/0620/r1188789_1296x729_16-9.jpg",
+}
+]
